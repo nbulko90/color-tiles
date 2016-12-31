3397fc8cd0367852a324f85b23fa9b0cdec1e401
Nick Bulko <nbulko90@gmail.com>
2016-12-31 11:21:30 -0500

    Completed grayscale version

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
 	
-	//var gridsize = prompt("Please specify the width of the tiles: ");
+	var numTiles = prompt("Please specify the number of tiles across/down: ");
+	
+	populate(numTiles);
+	init();
 	
 	$("#clear").click(function(){
 		$("#container").css("background-color", "purple");
 	});
 
+function populate(numTiles){
+
+	var tileSize = 800 / numTiles;
+
+	for(var i=0; i<numTiles*numTiles; i++){
+		$("#container").append("<div class='tile'></div>");
+	}
+
+	$(".tile").css("height", tileSize);
+	$(".tile").css("width", tileSize);
+	$(".tile").css("background-color", "black");
+}
+
+function init(){
+	$(".tile").hover(function(){
+		$(this).css("opacity", "+=.1");
+	})
+}
 	
 });
